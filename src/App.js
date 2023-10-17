7a2c7fa26a62e7d5570cd45db44cc6f6c42e1608
Djimo van Berlo <djimovanberlo@gmail.com>
2023-10-17 13:47:59 +0200

    yes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, IconButton, Typography } from '@mui/material'
+import { useState } from 'react'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import ReplayIcon from '@mui/icons-material/Replay'
+import './App.css'
 
 function App() {
+  const matches = useMediaQuery('(min-width:600px)')
+  const [state, setState] = useState('')
+
+  const clickFoto = () => {
+    if (state !== '') return
+    setState('foto')
+  }
+
+  const clickVideo = () => {
+    if (state !== '') return
+    setState('video')
+  }
+
+  const clickRefresh = () => {
+    window.location.reload(false)
+  }
+
+  const fotoVariant = state !== 'foto' ? 'outlined' : 'contained'
+  const videoVariant = state !== 'video' ? 'outlined' : 'contained'
+
+  const fotoColor = state === 'foto' ? 'success' : 'primary'
+  const videoColor = state === 'video' ? 'error' : 'primary'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className='app'>
+      <div className='wrapper'>
+        <img src='https://picsum.photos/200' />
+        <Typography variant={matches ? 'h3' : 'h4'}>Foto of video?</Typography>
+        <span>
+          <Button variant={fotoVariant} color={fotoColor} onClick={clickFoto}>
+            foto
+          </Button>
+          <Button variant={videoVariant} color={videoColor} onClick={clickVideo}>
+            video
+          </Button>
+        </span>
+        {state !== '' && (
+          <IconButton className='refresh' onClick={clickRefresh} variant='contained' color='primary'>
+            <ReplayIcon />
+          </IconButton>
+        )}
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
